eb4fe03b35320103495f6ead479dfdc56e82a6c6
bob <vasyaovosh@gmail.com>
2021-12-25 21:40:27 +0800

    Deploy on Heroku

--- learning/settings.py
+++ learning/settings.py
@@ -13,11 +13,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
